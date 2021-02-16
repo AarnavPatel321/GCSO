77a865cf18585dec859c0bdb88e6bc82e84d720e
AarnavPatel321 <73790531+AarnavPatel321@users.noreply.github.com>
2021-02-15 18:16:20 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var speed;
+var weight;
+var car;
+var wall;
+function setup() {
+  createCanvas(1600,400);
+  createSprite(400, 200, 50, 50);
+  speed=random(55,90);
+  weight=random(400,1500)
+}
+
+function draw() {
+  background(255,255,255);  
+  car=createSprite(50,200,50,50);
+  wall=createSprite(1500,200,60,height/2);
+  car.velocityX=speed;
+  drawSprites();
+}
